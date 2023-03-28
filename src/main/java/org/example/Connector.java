df3b203c578a17f2a36a03aeda9d23ada41de086
MichelleMwamba <s224573@student.dtu.dk>
2023-03-28 16:34:28 +0200

    Informationer om Databasen

--- src/main/java/org/example/Connector.java
+++ src/main/java/org/example/Connector.java
@@ -2,34 +2,40 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class Connector {
     private static final String HOST = "localhost";
 
-    private static final int PORT = "oooo";
+    private static final String PORT = "3306";
 
     private static final String USERNAME = "root";
 
-    private static final String DATABASE = "DatabaseNavn";
+    private static final String DATABASE = "TV3";
 
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = " ";
 
     private static final String DELIMITER = ";;";
 
     private Connection connection = null;
 
     Connector() {
+        String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE + "?characterEncoding=" + connection;
         try {
-            String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE + "?serverTimezone=UTC";
-            Connection connection = DriverManager.getConnection(ulr, USERNAME, PASSWORD);
-
-            System.out.println ("Connection made");
-        } catch (SQLExeption | IllegalArgumentException ex ) {
-            ex.printStackTrace(System.err);
-        } finally {
-            if (connection == null) System.exit( -1);
+            Scanner scanner = new Scanner(System.in, "CP850"); //Western Europe Console CodePage
+            System.out.println("Type sql manipulation: ");
+            String sqlManipulation = scanner.nextLine();
+            scanner.close();
+            // Get a connection.
+            Connection connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+            // Create and execute Update.
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlManipulation);
+            // Close connection.
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
 
-    Connection getConnection(){return connection; }
-}
+    }}
